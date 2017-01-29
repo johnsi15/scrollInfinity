0eeeef2198000cc24ba4e4db6d890e63765e423e
john andrey <andrey_201015@hotmail.com>
2017-01-29 15:31:57 -0500

    Cambios en el server y html

---
+++ data.js
@@ -0,0 +1,17 @@
+var dataJson = { 'pages':[
+				{'page': '1' 'title': 'Hola mundo', 'body': 'Texto Texto'},
+				{'page': '2' 'title': 'Hola mundo2', 'body': 'Texto Texto2'},
+				{'page': '3' 'title': 'Hola mundo3', 'body': 'Texto Texto3'}
+				]};
+
+// function getdata(){
+
+//     json.chk = [];
+//     $('input:checked').each(function(i){
+//         json.chk.push({ val : $(this).val()});
+//     });
+
+//     json.txt = document.getElementById("txt").value;
+
+//     return json;
+// }
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+// var bodyParser = require('body-parser');
+
+app.use('*', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   next();
+});
+
+app.get('/api/notas/:page', function(req, res){
+  var dataJson = [{
+                  'title': 'Hola soy el titulo numero'+req.params.page,
+                  'body': 'Hola soy el body numero'+req.params.page
+                }
+              ]
+  res.send(dataJson);
+})
+
+app.listen(3000, function (err){
+  if (err) return console.log('Hubo un error'), process.exit(1);//Devolvemos un mensaje si existe algun error
+
+  console.log('Escuchando en el puerto 3000');
+});
